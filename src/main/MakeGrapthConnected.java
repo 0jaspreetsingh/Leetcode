833d4b6e4ee4c8ba09168392d14160c5179973fc
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2020-09-06 15:27:31 +0530

    leetcode problem no 8

--- src/main/MakeGrapthConnected.java
+++ src/main/MakeGrapthConnected.java
@@ -7,56 +7,79 @@ import java.util.List;
 
 /**
  * https://leetcode.com/problems/number-of-operations-to-make-network-connected/
- * 
- * @author jaspreetsingh03
  *
+ * @author jaspreetsingh03
  */
 public class MakeGrapthConnected {
 
-	// n computers atleast require n-1 connections to connect each other
-	public int makeConnected(int n, int[][] connections) {
-		if (connections.length < n - 1) {
-			return -1;
-		}
+    // n computers atleast require n-1 connections to connect each other
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) {
+            return -1;
+        }
 
-		List<HashSet<Integer>> li = new ArrayList<>();
+        List<HashSet<Integer>> li = new ArrayList<>();
 
-		for (int i = 0; i < n; i++) {
-			HashSet<Integer> temp = new HashSet<Integer>();
-			temp.add(i);
-			li.add(i, temp);
-		}
+        for (int i = 0; i < n; i++) {
+            HashSet<Integer> temp = new HashSet<Integer>();
+            temp.add(i);
+            li.add(i, temp);
+        }
 
-		for (int i = 0; i < connections.length; i++) {
+        for (int i = 0; i < connections.length; i++) {
 
-			int p = connections[i][0];
-			int q = connections[i][1];
+            int p = connections[i][0];
+            int q = connections[i][1];
+            join(li, p, q);
 
-			join(li, p, q);
+        }
 
-		}
-		
-		System.out.println(li);
+        System.out.println(li);
 
-		return 0;
-	}
+        List<HashSet<Integer>> ans = new ArrayList<>();
 
 
+        for (HashSet<Integer> hashSet : li) {
+            findAndJoin(hashSet, ans);
+        }
 
-	private void join(List<HashSet<Integer>> li, int p, int q) {
+        System.out.println(ans);
+        return 0;
+    }
 
-		HashSet<Integer> temp1 = li.get(p);
-		HashSet<Integer> temp2 = li.get(q);
+    private void findAndJoin(HashSet<Integer> hashSet, List<HashSet<Integer>> ans) {
 
-		temp1.addAll(temp2);
-		temp2.addAll(temp1);
-	}
+        for (HashSet<Integer> ansHash : ans) {
+            for (int val : hashSet) {
+                System.out.println(val);
+                if (ans.contains(val)) {
+                    System.out.println(ans);
+                    ansHash.addAll(hashSet);
+                    return;
+                }
+            }
+        }
+        System.out.println("Hello");
+        HashSet<Integer> temp = new HashSet<>();
+        temp.addAll(hashSet);
+        ans.add(temp);
+    }
 
-	public static void main(String[] args) {
 
-		int n = 4, connections[][] = { { 0, 1 }, { 0, 2 }, { 1, 2 } };
+    private void join(List<HashSet<Integer>> li, int p, int q) {
 
-		MakeGrapthConnected mgc = new MakeGrapthConnected();
-		System.out.println(mgc.makeConnected(n, connections));
-	}
+        HashSet<Integer> temp1 = li.get(p);
+        HashSet<Integer> temp2 = li.get(q);
+
+        temp1.addAll(temp2);
+        temp2.addAll(temp1);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 7, connections[][] = {{0, 1}, {0, 2}, {1, 2}, {1, 6}, {2, 6}, {3, 4}};
+
+        MakeGrapthConnected mgc = new MakeGrapthConnected();
+        System.out.println(mgc.makeConnected(n, connections));
+    }
 }
