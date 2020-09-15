767219cbc638be42a608308ce82bd6340b58a791
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2020-09-16 00:36:38 +0530

    1319. Number of Operations to Make Network Connected

--- src/main/BestTimetoBuyandSellStockII.java
+++ src/main/BestTimetoBuyandSellStockII.java
@@ -1,28 +1,30 @@
 package main;
 
-// https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3287/
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ */
 
 public class BestTimetoBuyandSellStockII {
 
-	public int maxProfit(int[] prices) {
-		boolean bought = false;
-		boolean sold = false;
+    public int maxProfit(int[] prices) {
+        boolean bought = false;
+        boolean sold = false;
 
-		int profitSoFar = 0;
+        int profitSoFar = 0;
 
-		for (int i = 1; i < prices.length; i++) {
-			if (prices[i] - prices[i - 1] > 0) {
-				profitSoFar += prices[i] - prices[i - 1];
-			}
-		}
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                profitSoFar += prices[i] - prices[i - 1];
+            }
+        }
 
-		return profitSoFar;
-	}
+        return profitSoFar;
+    }
 
-	public static void main(String args[]) {
-		BestTimetoBuyandSellStockII dc = new BestTimetoBuyandSellStockII();
-		int prices[] = { 7, 1, 5, 3, 6, 4 };
-		System.out.println(dc.maxProfit(prices));
-	}
+    public static void main(String args[]) {
+        BestTimetoBuyandSellStockII dc = new BestTimetoBuyandSellStockII();
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.out.println(dc.maxProfit(prices));
+    }
 
 }
--- src/main/MakeGrapthConnected.java
+++ src/main/MakeGrapthConnected.java
@@ -1,11 +1,9 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
+ * 1319. Number of Operations to Make Network Connected
  * https://leetcode.com/problems/number-of-operations-to-make-network-connected/
  *
  * @author jaspreetsingh03
@@ -18,66 +16,46 @@ public class MakeGrapthConnected {
             return -1;
         }
 
-        List<HashSet<Integer>> li = new ArrayList<>();
-
+        int parent[] = new int[n];
         for (int i = 0; i < n; i++) {
-            HashSet<Integer> temp = new HashSet<Integer>();
-            temp.add(i);
-            li.add(i, temp);
+            parent[i] = i;
         }
-
+        int components = 0;
+        int extra = 0;
         for (int i = 0; i < connections.length; i++) {
-
-            int p = connections[i][0];
-            int q = connections[i][1];
-            join(li, p, q);
-
-        }
-
-        System.out.println(li);
-
-        List<HashSet<Integer>> ans = new ArrayList<>();
-
-
-        for (HashSet<Integer> hashSet : li) {
-            findAndJoin(hashSet, ans);
-        }
-
-        System.out.println(ans);
-        return 0;
-    }
-
-    private void findAndJoin(HashSet<Integer> hashSet, List<HashSet<Integer>> ans) {
-
-        for (HashSet<Integer> ansHash : ans) {
-            for (int val : hashSet) {
-                System.out.println(val);
-                if (ans.contains(val)) {
-                    System.out.println(ans);
-                    ansHash.addAll(hashSet);
-                    return;
-                }
+            int p1 = findParent(parent, connections[i][0]);
+            int p2 = findParent(parent, connections[i][1]);
+            if (p1 == p2) {
+                extra++;
+            } else {
+                parent[p1] = p2;
             }
         }
-        System.out.println("Hello");
-        HashSet<Integer> temp = new HashSet<>();
-        temp.addAll(hashSet);
-        ans.add(temp);
+
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                components++;
+            }
+        }
+
+        return components - 1 <= extra ? components - 1 : -1;
     }
 
-
-    private void join(List<HashSet<Integer>> li, int p, int q) {
-
-        HashSet<Integer> temp1 = li.get(p);
-        HashSet<Integer> temp2 = li.get(q);
-
-        temp1.addAll(temp2);
-        temp2.addAll(temp1);
+    private int findParent(int parent[], int index) {
+        if (index == parent[index]) {
+            return index;
+        }
+        return findParent(parent, parent[index]);
     }
 
     public static void main(String[] args) {
 
-        int n = 7, connections[][] = {{0, 1}, {0, 2}, {1, 2}, {1, 6}, {2, 6}, {3, 4}};
+        /**
+         * 10
+         * [[6,8],[0,4],[1,2],[5,8],[0,9],[1,8],[1,4],[4,9],[4,6],[3,7],[2,4],[3,5],[6,7],[4,5]]
+         */
+
+        int n = 5, connections[][] = {{0, 1}, {0, 2}, {3, 4}, {2, 3}};
 
         MakeGrapthConnected mgc = new MakeGrapthConnected();
         System.out.println(mgc.makeConnected(n, connections));
