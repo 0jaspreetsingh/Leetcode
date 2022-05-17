3d4f3dd0658b99330b4fb1856a6e3b81e059dcfe
Jaspreet Singh <jaspreetsingh1051997@gmail.com>
2022-05-17 12:56:40 +0200

    test commit
    Signed-off-by: Jaspreet Singh <jaspreetsingh1051997@gmail.com>

--- src/Greedy/MostProfitAssignedWork.java
+++ src/Greedy/MostProfitAssignedWork.java
@@ -21,7 +21,6 @@ public class MostProfitAssignedWork {
             return "profit: " + profit + " difficulty: " + difficulty;
         }
     }
-// test
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         int size = difficulty.length;
         Work[] workList = new Work[size];
